75094c1252796db477228d5227a7a103869fd430
taoyimin <1103995288@qq.com>
2019-05-06 20:37:35 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.TreeMap;
+
+/**
+ * @author Taoyimin
+ * @create 2019 05 06 17:31
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello world");
+    }
+}
