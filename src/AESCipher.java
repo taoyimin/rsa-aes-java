343a6b59605a0f9f1a6f81907b8fbca853e9600a
taoyimin <1103995288@qq.com>
2019-05-07 22:07:51 +0800

    add AESCipher class

---
+++ src/AESCipher.java
@@ -0,0 +1,51 @@
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+/**
+ * @author Taoyimin
+ * @create 2019 05 07 12:31
+ */
+public class AESCipher {
+
+    /**
+     * 加密方法，使用key充当向量iv，增加加密算法的强度
+     *
+     * @param key 密钥
+     * @param raw 需要加密的内容
+     * @return
+     */
+    public static String encrypt(byte[] key, String raw) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+        byte[] enCodeFormat = secretKey.getEncoded();
+        SecretKeySpec seckey = new SecretKeySpec(enCodeFormat, "AES");
+        Cipher cipher = Cipher.getInstance(ConfigureEncryptAndDecrypt.AES_ALGORITHM);
+        //使用key充当向量iv，增加加密算法的强度
+        IvParameterSpec iv = new IvParameterSpec(key);
+        cipher.init(Cipher.ENCRYPT_MODE, seckey, iv);
+        byte[] result = cipher.doFinal(raw.getBytes());
+        Base64.Encoder encoder = Base64.getEncoder();
+        return encoder.encodeToString(result);
+    }
+
+    /**
+     * 解密方法，使用key充当向量iv，增加加密算法的强度
+     *
+     * @param key 密钥
+     * @param enc 待解密内容
+     * @return
+     */
+    public static String decrypt(byte[] key, String enc) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+        byte[] enCodeFormat = secretKey.getEncoded();
+        SecretKeySpec seckey = new SecretKeySpec(enCodeFormat, "AES");
+        Cipher cipher = Cipher.getInstance(ConfigureEncryptAndDecrypt.AES_ALGORITHM);
+        //
+        IvParameterSpec iv = new IvParameterSpec(key);
+        cipher.init(Cipher.DECRYPT_MODE, seckey, iv);
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] result = cipher.doFinal(decoder.decode(enc));
+        return new String(result);
+    }
+}
