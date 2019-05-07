0b3b94b1e4acf33add7d4c0a3309848be496886c
taoyimin <1103995288@qq.com>
2019-05-07 22:08:37 +0800

    regular update

---
+++ src/ConfigureEncryptAndDecrypt.java
@@ -0,0 +1,7 @@
+public class ConfigureEncryptAndDecrypt {
+	public static final String CHAR_ENCODING = "UTF-8";
+	public static final String AES_ALGORITHM = "AES/CBC/PKCS5Padding";
+	//public static final String RSA_ALGORITHM = "RSA/ECB/PKCS1Padding";
+	public static final String RSA_ALGORITHM = "RSA/ECB/OAEPWithSHA-256AndMGF1Padding";
+}
+
