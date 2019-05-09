2533f79620a6f6133ae1de48f78a0f6903ddc171
taoyimin <1103995288@qq.com>
2019-05-09 11:49:47 +0800

    delete useless class

--- src/ConfigureEncryptAndDecrypt.java
+++
@@ -1,7 +0,0 @@
-public class ConfigureEncryptAndDecrypt {
-	public static final String CHAR_ENCODING = "UTF-8";
-	public static final String AES_ALGORITHM = "AES/CBC/PKCS5Padding";
-	//public static final String RSA_ALGORITHM = "RSA/ECB/PKCS1Padding";
-	public static final String RSA_ALGORITHM = "RSA/ECB/OAEPWithSHA-256AndMGF1Padding";
-}
-
